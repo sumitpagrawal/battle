52408bee289217e431e782e7e27259bb2deff39d
sumitpagrawal <sumitpagrawal@gmail.com>
2018-04-18 18:23:26 +0530

    S5L46:Working HP bars

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ from classes.inventory import Item
 
 
 #Create Black Magic
-fire = Spell("Fire", 10, 100, "black")
-thunder = Spell("Thunder", 10, 100, "black")
-blizzard = Spell("Blizzard", 10, 100, "black")
-meteor = Spell("Meteor", 20, 200, "black")
+fire = Spell("Fire", 25, 600, "black")
+thunder = Spell("Thunder", 25, 600, "black")
+blizzard = Spell("Blizzard", 25, 600, "black")
+meteor = Spell("Meteor", 40, 1200, "black")
 quake = Spell("Quake", 14, 140, "black")
 
 
 #create While magic
-cure = Spell("Cure", 12, 120, "White")
-cura = Spell("Cura", 18, 200, "White")
+cure = Spell("Cure", 25, 620, "White")
+cura = Spell("Cura", 32, 1500, "White")
 
 
 #Create some Items
 potion = Item("Potion", "potion", "Heals 50 HP", 50)
 hipotion = Item("Hi-Potion", "potion", "Heals 100 HP", 100)
-superpotion = Item("Super Potion", "potion", "Heals 500 HP", 500)
+superpotion = Item("Super Potion", "potion", "Heals 500 HP", 1000)
 elixer = Item("Elixer", "elixer", "Fully restores HP/MP of one party member", 9999)
 hielixer = Item("MegaElixer", "elixer", "Fully restores party's HP/MP", 9999)
 grenade = Item("Grenade", "attack","Deals 500 damage", 500)
@@ -30,10 +30,10 @@ player_items = [{"item": potion, "quantity":5}, {"item": hipotion, "quantity":5}
                 {"item": hielixer, "quantity":5},{"item": grenade, "quantity":5}]
 
 #Instantiate People
-player1 = person("Valos", 3260,65,60, 34, player_spells,player_items)
-player2 = person("Nick ", 4160,65,60, 34, player_spells,player_items)
-player3 = person("Robot", 3089,65,60, 34, player_spells,player_items)
-enemy = person("Magus",1200, 65,45, 25, [],[])
+player1 = person("Valos", 3260,132, 300, 34, player_spells,player_items)
+player2 = person("Nick ", 4160,188, 311, 34, player_spells,player_items)
+player3 = person("Robot", 3089,174, 288, 34, player_spells,player_items)
+enemy = person("Magus",11200, 701, 525, 25, [],[])
 
 players = [player1, player2, player3]
 running = True
@@ -44,7 +44,7 @@ while running:
     print("==========================")
 
     print("\n\n")
-    print("NAME                      HP                          |MP")
+    print("NAME                      HP                                |MP")
 
     for player in players:
         player.get_stats()
